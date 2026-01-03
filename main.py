51e6e1141c6bec6cc82aee46289d9648efb095a3
r3tten-gutzzz <mawahibfateema@gmail.com>
2026-01-03 12:27:05 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,30 +1,68 @@
-import requests
+from transformers import pipeline
+from colorama import Fore, Style, init 
 
-def get_random_joke():
-    url = "https://official-joke-api.appspot.com/random_joke"
-    response = requests.get(url)
+init(autoreset=True)
 
-    if response.status_code == 200:
+classifier = pipeline(
 
-        joke_data = response.json()
-        return f"{joke_data['setup']} - {joke_data['punchline']}"
+task="sentiment-analysis",
+
+model="distilbert-base-uncased-finetuned-sst-2-english"
+
+)
+total_count = 0
+positive_count = 0
+negative_count = 0
+
+print(Fore.CYAN + Style.BRIGHT + "\nAI SENTIMENT ANALYSIS SYSTEM")
+print(Fore.CYAN + "-" * 35)
+print(Fore.YELLOW + "Enter any sentence to analyze sentiment")
+print(Fore.YELLOW + "Type 'Exit' to terminate the system\n")
+
+while True:
+    user_input = input(Fore.WHITE + "Input Text: ").strip()
+
+    if user_input.lower() =="exit":
+        print(Fore.CYAN + "\nSession Summary")
+        print(Fore.CYAN + "-" * 35)
+        print(Fore.GREEN + f"Total Inputs  : {total_count}")
+        print(Fore.GREEN + f"Positive      : {positive_count}")
+        print(Fore.RED + f"Negative       : {negative_count}")
+        print(Fore.CYAN + "\nSystem closed succesfully.\n")
+        break
+
+    if not user_input:
+        print(Fore.RED + "Input cannot be empty\n")
+        continue
+
+    result = classifier(user_input)[0]
+    label = result["label"]
+    confidence = round(result["score"] * 100, 2)
+
+    total_count += 1
+
+    if confidence >= 85:
+        strength = "strong"
+    elif confidence >= 65:
+        strength = "Moderate"
+    else: 
+        strength = "weak"
+    
+    if label == "POSITIVE":
+        positive_count += 1 
+        color = Fore.GREEN
     else:
-        return "Failed to retreive joke."
+        negative_count += 1
+        color = Fore.RED
+    
+    print(Fore.CYAN + "\nAnalaysis result")
+    print(Fore.CYAN + "-" * 35)
+    print(Fore.GREEN + f"Sentement : {label}")
+    print(Fore.GREEN + f"Confidence     : {confidence}")
+    print(Fore.RED + f"Strength level     : {strength}")
 
-def main():
-    print("Welcome to random joke generator!")
 
-    while True:
-        userInput = input("Do you want to get a new joke? Y/N: ").strip().lower()
-        
-        if userInput in ["no", "n"]:
-            print("Goodbye")
-            break
 
-        joke = get_random_joke()
-        print(joke)
 
-if __name__== "__main__":
-    main()
 
 
