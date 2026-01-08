2c9d151be54d501aedc30668b9a52aa23b7f2210
r3tten-gutzzz <mawahibfateema@gmail.com>
2026-01-08 22:22:50 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,68 +1,58 @@
 from transformers import pipeline
-from colorama import Fore, Style, init 
+from colorama import Fore, Style, init
+import torch 
 
 init(autoreset=True)
 
-classifier = pipeline(
+class BartTextSummarizer:
+    def __init__(self):
+        self.device = 0 if torch.cuda.is_available() else - 1
+        self.model_name = "facebook/bart-large-cnn"
 
-task="sentiment-analysis",
-
-model="distilbert-base-uncased-finetuned-sst-2-english"
-
-)
-total_count = 0
-positive_count = 0
-negative_count = 0
-
-print(Fore.CYAN + Style.BRIGHT + "\nAI SENTIMENT ANALYSIS SYSTEM")
-print(Fore.CYAN + "-" * 35)
-print(Fore.YELLOW + "Enter any sentence to analyze sentiment")
-print(Fore.YELLOW + "Type 'Exit' to terminate the system\n")
-
-while True:
-    user_input = input(Fore.WHITE + "Input Text: ").strip()
-
-    if user_input.lower() =="exit":
-        print(Fore.CYAN + "\nSession Summary")
-        print(Fore.CYAN + "-" * 35)
-        print(Fore.GREEN + f"Total Inputs  : {total_count}")
-        print(Fore.GREEN + f"Positive      : {positive_count}")
-        print(Fore.RED + f"Negative       : {negative_count}")
-        print(Fore.CYAN + "\nSystem closed succesfully.\n")
-        break
-
-    if not user_input:
-        print(Fore.RED + "Input cannot be empty\n")
-        continue
-
-    result = classifier(user_input)[0]
-    label = result["label"]
-    confidence = round(result["score"] * 100, 2)
-
-    total_count += 1
-
-    if confidence >= 85:
-        strength = "strong"
-    elif confidence >= 65:
-        strength = "Moderate"
-    else: 
-        strength = "weak"
+        self.summarizer = pipeline(
+            "summarization",
+            model=self.model_name,
+            device=self.device
+        )
     
-    if label == "POSITIVE":
-        positive_count += 1 
-        color = Fore.GREEN
-    else:
-        negative_count += 1
-        color = Fore.RED
+    def summarize(self, text):
+        input_length = len(text.split())
+
+        max_length = max(30, int(input_length * 0.6))
+        min_length = max(30, int(max_length * 0.5))
+
+        summary = self.summarizer(
+            text,
+            min_length=min_length,
+            max_length=max_length,
+            do_sample=False
+        )
+
+        return summary[0]["Summary text"]
     
-    print(Fore.CYAN + "\nAnalaysis result")
-    print(Fore.CYAN + "-" * 35)
-    print(Fore.GREEN + f"Sentement : {label}")
-    print(Fore.GREEN + f"Confidence     : {confidence}")
-    print(Fore.RED + f"Strength level     : {strength}")
+
+def main():
+    print(Fore.CYAN + Style.BRIGHT + "\nLLm Text Summarization System\n")
+
+    user_name = input("Enter your name: ").strip() or "User"
+    print(Fore.GREEN + f"\nWelcome, {user_name}")
+
+    print("\nEnter text to summarize:")
+    text = input("> ").strip()
+
+    if not text:
+        print(Fore.RED + "No input provided. Exiting...")
+        return
+    
+    print(Fore.BLUE + "\nIntilizing BART model...")
+    engine = BartTextSummarizer()
+
+    print(Fore.BLUE + "Performing summarization..\n")
+    summary = engine.summarize(text)
+
+    print(Fore.GREEN + Style.BRIGHT + f"Summary Output for {user_name}:\n")
+    print(summary)
 
 
-
-
-
-
+if __name__ == "__main__":
+    main()
